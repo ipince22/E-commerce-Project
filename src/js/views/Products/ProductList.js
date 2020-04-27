aa13251894c7ce0403a69a0d8affb0813df97501
Daniel Ipince <ipince22@gmail.com>
2020-04-27 23:36:46 +0000

    04.27 daniel

--- src/js/views/Products/ProductList.js
+++ src/js/views/Products/ProductList.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { Product } from "./Product";
 import { ProductConsumer } from "../../store/context";
+import img1 from "../../../img/c1.jpg";
 export const ProductList = () => {
 	return (
 		<React.Fragment>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-lg-12 col-lg-4 text-center">
+						<img
+							src={img1}
+							className="img-fluid max-width: 100% height: auto;"
+						/>
+					</div>
+				</div>
+			</div>
+
 			<div className="py-5">
 				<div className="container">
 					{/*<Title name="our" title="products" />*/}
@@ -32,6 +44,33 @@ export const ProductList = () => {
 								});
 							}}
 						</ProductConsumer>
+					</div>
+
+					<div className="row">
+						<div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+							<div className="video-container">
+								<iframe
+									width="100%"
+									height="315"
+									src="https://www.youtube.com/embed/H4p6njjPV_o"
+									frameBorder="0"
+									allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+									allowFullScreen
+								/>
+							</div>
+						</div>
+						<div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+							<div className="video-container">
+								<iframe
+									width="100%"
+									height="315"
+									src="https://www.youtube.com/embed/SQIbeAk-bFA"
+									frameBorder="0"
+									allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+									allowFullScreen
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
